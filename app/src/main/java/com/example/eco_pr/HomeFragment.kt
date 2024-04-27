31cd2a35ef03cd4d1347f5cd8f6d793e7b1ce678
Makar <makarsyshchikov04@gmail.com>
2024-04-27 11:24:30 +0300

    firebase is here

--- app/src/main/java/com/example/eco_pr/HomeFragment.kt
+++ app/src/main/java/com/example/eco_pr/HomeFragment.kt
@@ -1,11 +1,16 @@
 package com.example.eco_pr
 
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.example.eco_pr.databinding.FragmentHomeBinding
+import com.google.firebase.firestore.FirebaseFirestore
+import com.uber.h3core.H3Core
+import java.text.SimpleDateFormat
+import java.util.*
 
 private lateinit var binding: FragmentHomeBinding
 class HomeFragment : Fragment() {
@@ -16,7 +21,23 @@ class HomeFragment : Fragment() {
     ): View? {
         binding = FragmentHomeBinding.inflate(inflater,container,false)
         val view = binding.root
+        val currentDate = SimpleDateFormat("dd.MM.yyyy", Locale.getDefault()).format(Date())
+        val db = FirebaseFirestore.getInstance()
+        val data = hashMapOf(
+            "address" to "Da",
+            "sound" to "Ga"
+        )
 
+
+        db.collection("sound")
+            .document(currentDate)
+            .set(data)
+            .addOnSuccessListener {
+                // DocumentSnapshot added with ID: documentReference.id
+            }
+            .addOnFailureListener { e ->
+                Log.e("Firestore", "Failed to write document", e)
+            }
 
         return view
     }
