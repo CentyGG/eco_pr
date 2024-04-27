c491b56f106c6b99d3bb81d0ba93b9e225145c82
Makar <makarsyshchikov04@gmail.com>
2024-04-27 17:20:29 +0300

    Сделал красивый activityMain

--- app/src/main/java/com/example/eco_pr/MainActivity.kt
+++ app/src/main/java/com/example/eco_pr/MainActivity.kt
@@ -1,15 +1,15 @@
 package com.example.eco_pr
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Gravity
+import android.view.View
+import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.NavController
-import androidx.navigation.findNavController
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.fragment.findNavController
-import androidx.navigation.ui.setupWithNavController
 import com.example.eco_pr.databinding.ActivityMainBinding
-import com.example.eco_pr.databinding.FragmentNoiseBinding
 import com.google.android.material.bottomnavigation.BottomNavigationView
+import com.google.android.material.bottomsheet.BottomSheetDialog
 
 class MainActivity : AppCompatActivity() {
     private lateinit var bottomNav: BottomNavigationView
@@ -22,10 +22,25 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        bottomNav = binding.bottomBar
         navHostFragment = supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
         navController = navHostFragment.findNavController()
-        bottomNav.setupWithNavController(navController)
+
+        binding.searchB.setOnClickListener(object : View.OnClickListener{
+            override fun onClick(p0: View?) {
+                var dialog = BottomSheetDialog(this@MainActivity)
+                dialog.setContentView(R.layout.search_sheet_dialog)
+                dialog.window?.setGravity(Gravity.BOTTOM)
+                dialog.show()
+            }
+        })
+        binding.layerB.setOnClickListener(object : View.OnClickListener{
+            override fun onClick(p0: View?) {
+                var dialog = BottomSheetDialog(this@MainActivity)
+                dialog.setContentView(R.layout.layer_sheet_dialog)
+                dialog.window?.setGravity(Gravity.BOTTOM)
+                dialog.show()
+            }
+        })
 
     }
 }
